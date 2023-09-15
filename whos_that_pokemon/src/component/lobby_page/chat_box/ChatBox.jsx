dde097838bb865b81b481bcc269c4c477e1b8212
Amrit Thapa <realamritthapa@gmail.com>
2023-09-15 13:46:50 -0700

    made the code more managable by taking out some div

--- whos_that_pokemon/src/component/lobby_page/chat_box/ChatBox.jsx
+++ whos_that_pokemon/src/component/lobby_page/chat_box/ChatBox.jsx
@@ -23,29 +23,27 @@ export default function ChatBox({ data }) {
 
   return (
     <div className='chat-box'>
-      <div className='row d-flex justify-content-center w-75'>
-        <div className='message-box  '>
-          <div className='message-container'>
-            <Messages />
-          </div>
-          <form className='row' onSubmit={handleSubmit}>
-            <div className='col-md-10 form-group  '>
-              {/* <label for='message-input'></label> */}
-              <textarea
-                className='w-100 h-100'
-                type='text'
-                id='message-input'
-                placeholder='type here'
-                value={message}
-                onChange={handleChange}
-              ></textarea>
-            </div>
-
-            <button type='submit' className='col btn btn-primary btn-sm '>
-              Send
-            </button>
-          </form>
+      <div className='message-box  '>
+        <div className='message-container'>
+          <Messages />
         </div>
+        <form className='form-style row' onSubmit={handleSubmit}>
+          <div className='col-md-10 form-group  '>
+            {/* <label for='message-input'></label> */}
+            <textarea
+              className='w-100 h-100'
+              type='text'
+              id='message-input'
+              placeholder='type here'
+              value={message}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+
+          <button type='submit' className='col btn btn-primary btn-sm '>
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
